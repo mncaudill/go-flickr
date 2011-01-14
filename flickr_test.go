56545ff91ecd4d66b1f4b34b43d91352dc591308
Nolan Caudill <nolan@nolancaudill.com>
2011-01-13 22:44:54 -0800

    Removing API key (and disabling key on Flickr's end, oops)

--- flickr_test.go
+++ flickr_test.go
@@ -6,13 +6,13 @@ import "fmt"
 
 func main() {
 	r := new(flickr.Request)
-	r.ApiKey = "e17940558f4af5df0933c5b7aac53de0"
+	r.ApiKey = "APIKEYHERE"
 	r.Method = "flickr.photos.getInfo"
 
 	r.Args = make(map[string]string, 10)
 	r.Args["photo_id"] = "5336400553"
 
-	r.Sign("e2a257c7891af67e")
+	r.Sign("APISECRETHERE")
 	_, err := r.Execute()
 	if err != nil {
 		fmt.Println("Something went wrong:", err)
