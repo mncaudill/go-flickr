aeaeabe54dc0c2f5ebf824e246acfa855915ea14
Nolan Caudill <nolan@nolancaudill.com>
2011-01-13 22:38:30 -0800

    Upload API now works

--- flickr.go
+++ flickr.go
@@ -1,16 +1,22 @@
 package flickr
 
 import (
+	"io"
 	"os"
 	"fmt"
+	"net"
 	"http"
 	"sort"
+	"bufio"
+	"bytes"
 	"io/ioutil"
 	"crypto/md5"
 )
 
 const (
-	endpoint = "http://api.flickr.com/services/rest/?"
+	endpoint        = "http://api.flickr.com/services/rest/?"
+	upload_endpoint = "http://api.flickr.com/services/upload/"
+	api_host        = "api.flickr.com"
 )
 
 type Request struct {
@@ -18,6 +24,12 @@ type Request struct {
 	Method string
 	Args   map[string]string
 }
+
+type nopCloser struct {
+	io.Reader
+}
+
+func (nopCloser) Close() os.Error { return nil }
 
 // So we can return custom errors
 type Error string
@@ -45,7 +57,9 @@ func (request *Request) Sign(secret string) {
 	// Build out ordered key-value string prefixed by secret
 	s := secret
 	for _, key := range sorted_keys {
-		s += fmt.Sprintf("%s%s", key, args[key])
+		if args[key] != "" {
+			s += fmt.Sprintf("%s%s", key, args[key])
+		}
 	}
 
 	// Since we're only adding two keys, it's easier 
@@ -72,15 +86,7 @@ func (request *Request) Execute() (response string, ret os.Error) {
 	args["api_key"] = request.ApiKey
 	args["method"] = request.Method
 
-	s := endpoint
-	i := 0
-	for k, v := range args {
-		if i != 0 {
-			s += "&"
-		}
-		i++
-		s += fmt.Sprintf("%s=%s", k, http.URLEscape(v))
-	}
+	s := endpoint + encodeQuery(args)
 
 	res, _, err := http.Get(s)
 	defer res.Body.Close()
@@ -89,5 +95,72 @@ func (request *Request) Execute() (response string, ret os.Error) {
 	}
 
 	body, _ := ioutil.ReadAll(res.Body)
+	return string(body), nil
+}
+
+func encodeQuery(args map[string]string) string {
+	s := ""
+	i := 0
+	for k, v := range args {
+		if i != 0 {
+			s += "&"
+		}
+		i++
+		s += fmt.Sprintf("%s=%s", k, http.URLEscape(v))
+	}
+	return s
+}
+
+func (request *Request) Upload(filename string) (response string, err os.Error) {
+	photo_file, error := ioutil.ReadFile(filename)
+	if error != nil {
+		return "", error
+	}
+
+	request.Args["api_key"] = request.ApiKey
+
+	boundary := "----###---###--flickr-go-rules"
+	end := "\r\n"
+
+	post_body := ""
+	for k, v := range request.Args {
+		post_body += "--" + boundary + end
+		post_body += "Content-Disposition: form-data; name=\"" + k + "\"" + end + end
+		post_body += v + end
+	}
+
+	post_body += "--" + boundary + end
+	post_body += "Content-Disposition: form-data; name=\"photo\"; filename=\"photo.jpg\"" + end
+	post_body += "Content-Type: image/jpeg" + end + end
+	post_body += string(photo_file) + end
+	post_body += "--" + boundary + "--" + end
+
+	post_req := new(http.Request)
+	post_req.Method = "POST"
+	post_req.RawURL = upload_endpoint
+	post_req.Host = api_host
+	post_req.Header = map[string]string{
+		"Content-Type": "multipart/form-data; boundary=" + boundary + end,
+	}
+
+	post_req.Body = nopCloser{bytes.NewBufferString(post_body)}
+	post_req.ContentLength = int64(len(post_body))
+
+	// Create and use TCP connection
+	conn, err := net.Dial("tcp", "", "api.flickr.com:80")
+	defer conn.Close()
+
+	if err != nil {
+		return "", err
+	}
+	post_req.Write(conn)
+
+	reader := bufio.NewReader(conn)
+	resp, err := http.ReadResponse(reader, post_req.Method)
+	if err != nil {
+		return "", err
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+
 	return string(body), nil
 }
--- flickr_test.go
+++ flickr_test.go
@@ -3,18 +3,21 @@ package main
 import "./flickr"
 import "fmt"
 
+
 func main() {
 	r := new(flickr.Request)
-	r.ApiKey = "APIKEYGOESHERE"
+	r.ApiKey = "e17940558f4af5df0933c5b7aac53de0"
 	r.Method = "flickr.photos.getInfo"
 
 	r.Args = make(map[string]string, 10)
 	r.Args["photo_id"] = "5336400553"
 
-	r.Sign("APISECRETGOESHERE")
-	body, err := r.Execute()
+	r.Sign("e2a257c7891af67e")
+	_, err := r.Execute()
 	if err != nil {
 		fmt.Println("Something went wrong:", err)
 	}
-	fmt.Println(body)
+//	fmt.Println(body)
+
+	r.Upload("thumb.jpg")
 }
